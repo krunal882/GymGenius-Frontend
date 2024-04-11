63776b9da6f0d5e00fef4da823be4e133359c5de
krunal882 <krunalvekariya254@gmail.com>
2024-04-11 22:27:31 +0530

    created pages for user's login-signup-resetPassword

---
+++ src/pages/fitness-trackers/FitnessTrackers.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img
+      width="100%"
+      src="../../assets/img/fitnessTrackers/Fitness-Calculators.png"
+      alt=""
+    />
+    <FitnessCard />
+  </div>
+  <router-view></router-view>
+</template>
+
+<script>
+import FitnessCard from "@/components/fitnessTracker/FitnessCard.vue";
+export default {
+  components: {
+    FitnessCard,
+  },
+};
+</script>
+
+<style></style>
