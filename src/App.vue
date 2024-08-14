822f1feacd7fb845b17f9cc0e828da8118aca838
Krunal Vekariya <130568099+krunal882@users.noreply.github.com>
2024-08-15 00:25:03 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>
-    <!-- Main navigation and footer are displayed only if the current route is not a special page -->
-    <main-nav-wrapper v-if="!isSpecialPage">
-      <footer-wrapper>
-        <!-- The main content of the page is displayed here -->
-        <router-view></router-view>
-      </footer-wrapper>
-    </main-nav-wrapper>
-    <!-- For special pages, only the main content is displayed without the main navigation and footer -->
-    <router-view v-if="isSpecialPage" />
+  <div class="app-container">
+    <!-- Left side ad -->
+    <div class="side-ad left-ad">
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-7764190062746470"
+           data-ad-slot="9707905683"
+           data-ad-format="auto"
+           data-full-width-responsive="true"></ins>
+    </div>
+
+    <!-- Main content with navigation and footer -->
+    <div class="main-content">
+      <main-nav-wrapper v-if="!isSpecialPage">
+        <footer-wrapper>
+          <router-view></router-view>
+        </footer-wrapper>
+      </main-nav-wrapper>
+      <router-view v-if="isSpecialPage" />
+    </div>
+
+    <!-- Right side ad -->
+    <div class="side-ad right-ad">
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-7764190062746470"
+           data-ad-slot="9707905683"
+           data-ad-format="auto"
+           data-full-width-responsive="true"></ins>
+    </div>
   </div>
 </template>
 
@@ -33,7 +53,32 @@ export default {
       );
     },
   },
+  mounted() {
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  },
 };
 </script>
 
-<style></style>
+<style>
+.app-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.main-content {
+  flex-grow: 1;
+  margin: 0 20px; /* Adjust as needed */
+}
+
+.side-ad {
+  width: 160px; /* Adjust the width as needed */
+}
+
+.left-ad {
+  /* You can add custom styles for the left ad if needed */
+}
+
+.right-ad {
+  /* You can add custom styles for the right ad if needed */
+}
+</style>
