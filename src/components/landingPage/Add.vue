0f315fd58d1e73f921b95f070c357cd30bb41feb
Krunal Vekariya <130568099+krunal882@users.noreply.github.com>
2024-08-17 12:11:16 +0530

    Create Add.vue

---
+++ src/components/landingPage/Add.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="ads-container">
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-format="autorelaxed"
+           data-ad-client="ca-pub-7764190062746470"
+           data-ad-slot="4174778919"></ins>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "GoogleAd",
+    mounted() {
+      if (window.adsbygoogle) {
+        window.adsbygoogle.push({});
+      } else {
+        const adScript = document.createElement('script');
+        adScript.async = true;
+        adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7764190062746470";
+        adScript.crossOrigin = "anonymous";
+        document.head.appendChild(adScript);
+  
+        adScript.onload = () => {
+          window.adsbygoogle = window.adsbygoogle || [];
+          window.adsbygoogle.push({});
+        };
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .ads-container {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  </style>
+  
